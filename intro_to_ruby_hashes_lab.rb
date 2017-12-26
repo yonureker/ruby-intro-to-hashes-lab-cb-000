8af4e9915e279fb1685fa7823646be8d79c37f0a
Onur Eker <yonureker@gmail.com>
2017-12-26 00:02:02 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,12 +3,14 @@ def new_hash
 end
 
 def actor
-
+	actor = {
+		:name => "Dwayne The Rock Johnson"
+	}
 end
 
 def monopoly
 	monopoly = {}
-
+	monopoly[:railroads] = {}
 end
 
 def monopoly_with_second_tier
